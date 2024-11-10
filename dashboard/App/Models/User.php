7563e87c90cac88f5271157d15d8f53f35f29f45
Ben <anibrikabenbright@gmail.com>
2024-11-10 09:14:06 +0000

    Initial commit

---
+++ dashboard/App/Models/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+class User 
+{
+  // public function greet (){
+  //   echo "Hello Samad?";
+
+  // }
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use App\Models\Booking;
+
+$user = new Booking();
+//$user->greet();
+
+
+
